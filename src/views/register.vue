6f9ca2d3b8b2e68f0c2805a125fd044f080285f9
stars <17343639593@163.com>
2021-09-04 16:32:34 +0800

    test eslint

--- src/views/register.vue
+++ src/views/register.vue
@@ -4,6 +4,10 @@
 
 <script lang="ts">
 import { Vue } from 'vue-class-component'
+console.log()
+console.log()
+console.log('q')
+console.log('w')
 
 export default class register extends Vue {}
 </script>
